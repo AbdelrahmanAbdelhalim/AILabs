e05ecb31913af1b0f5debb25c9b4f338ca95658b
AbdelrahmanAbdelhalim <56049226+AbdelrahmanAbdelhalim@users.noreply.github.com>
2021-02-17 19:32:34 +0000

    Working solution, still needs tweaking

--- lab1.py
+++ lab1.py
@@ -1,15 +1,40 @@
 import random as rand
+import itertools
+
+solutionsV = []
+solutionsS = []
 
 def genRandomNumbers():
-	ls = []
-	for i in range(4):			
-		x = rand.randint(1,13)
-		ls.append(x)
+	ls = [rand.randint(1,13) for i in range(4)]
 	return ls
 
-def solve24(ls):
-		
+def findPermutations(ls):
+	return list(itertools.permutations(ls))
+
+
+def solve24(permutation):
+	permutations = findPermutations(foo)
+	for permutation in permutations:
+		solve(permutation,0,[],[])
+	# for permutation in permutations:
+	# 	solve(permuatation[1:],ls[0])
+
+def solve(ls,total,values,signs):
+	if len(ls) == 0:
+		if total == 24:
+			print("found solution")
+			solutions.append(values)
+			solutions.append(signs)
+		return
+
+	solve(ls[1:],total + ls[0],values.append(total),signs.append("+"))
+	if ls[0] != 0:
+		solve(ls[1:],total / ls[0],values.append(total),signs.append("/"))
+	solve(ls[1:],total * ls[0],values.append(total),signs.append("x"))
+	solve(ls[1:],total - ls[0],values.append(total),signs.append("-"))
+
+
 
 if __name__ == "__main__":
 	foo = genRandomNumbers()
-	print(foo)
+	solve24(foo)
