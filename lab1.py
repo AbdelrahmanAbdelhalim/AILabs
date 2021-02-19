beef6fdcb759b07ee5bfcb7025ee3c7f017ccf23
AbdelrahmanAbdelhalim <56049226+AbdelrahmanAbdelhalim@users.noreply.github.com>
2021-02-19 08:28:10 +0000

    Working solution, Still needs saving solutions

--- lab1.py
+++ lab1.py
@@ -1,9 +1,6 @@
 import random as rand
 import itertools
 
-solutionsV = []
-solutionsS = []
-
 def genRandomNumbers():
 	ls = [rand.randint(1,13) for i in range(4)]
 	return ls
@@ -12,29 +9,48 @@ def findPermutations(ls):
 	return list(itertools.permutations(ls))
 
 
-def solve24(permutation):
-	permutations = findPermutations(foo)
-	for permutation in permutations:
-		solve(permutation,0,[],[])
-	# for permutation in permutations:
-	# 	solve(permuatation[1:],ls[0])
+def solve(ls,operation):	
+	if len(ls) > 1:
+		for l in range(len(ls)):
+			for r in range(len(ls)):
+				if l == r:
+					continue
+				newLs = []
+				for j in range(len(ls)):
+					if j != l and j!= r:
+						newLs.append(ls[j])
 
-def solve(ls,total,values,signs):
-	if len(ls) == 0:
-		if total == 24:
+				newLs.append(ls[l] + ls[r])
+				operation.append("plus")
+				solve(newLs,operation)
+				newLs = newLs[:-1]
+				operation = operation[:-1]
+
+				newLs.append(ls[l] - ls[r])
+				operation.append("minus")
+				solve(newLs,operation)
+				newLs = newLs[:-1]
+				operation = operation[:-1]
+
+				newLs.append(ls[l] * ls[r])
+				operation.append("mul")
+				solve(newLs,operation)
+				newLs = newLs[:-1]
+				operation = operation[:-1]
+
+				if ls[r] != 0:
+					newLs.append(ls[l] / ls[r])
+					operation.append("divide")
+					solve(newLs,operation)
+					newLs = newLs[:-1]
+					operation = operation[:-1]
+	if len(ls) == 1:
+		if abs(ls[0] - 24) < 0.0000001:
 			print("found solution")
-			solutions.append(values)
-			solutions.append(signs)
-		return
-
-	solve(ls[1:],total + ls[0],values.append(total),signs.append("+"))
-	if ls[0] != 0:
-		solve(ls[1:],total / ls[0],values.append(total),signs.append("/"))
-	solve(ls[1:],total * ls[0],values.append(total),signs.append("x"))
-	solve(ls[1:],total - ls[0],values.append(total),signs.append("-"))
-
-
+		else:
+			print(operation)
 
 if __name__ == "__main__":
 	foo = genRandomNumbers()
-	solve24(foo)
+	print(foo)
+	solve(foo,[])
