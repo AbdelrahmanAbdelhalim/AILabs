88703d6d297f0fe18fa1d2c8896c43cb6ede8229
Abdelraman Bahaaeldin <abdelrahmanbahaaeldin@gmail.com>
2021-02-25 04:37:42 +0000

    Recursion running, needs fixing

--- lab2.py
+++ lab2.py
@@ -23,12 +23,21 @@ def createGame():
 
 def calculateHeuristic(ls):
 	heuristic = 0
+	manDistance = 0
 	for i in range(len(ls)):
 		if ls[i] == 0:
 			continue
-		else:
-			if i + 1 != ls[i]:
-				heuristic += 1
+		if i + 1 != ls[i]:
+			heuristic += 1
+	for i in range(len(ls)):
+		if ls[i] == 0 :
+			continue
+		goalSquare = ls[i] - 1
+		diffY = abs((int(i/3) - int(goalSquare/3)))
+		diffX = abs((i%3 - goalSquare%3))
+		manDistance += (diffX + diffY)
+	print(manDistance)
+	heuristic += manDistance
 	return heuristic
 def calculateInversions(ls):
 	inversions = 0
@@ -51,46 +60,57 @@ def printBoard(ls):
 		if k % 3 == 0:
 			print()
 		print(ls[k],end = ' ')
+	print()
 def swapValuesInList(ls,index1,index2):
 	placeHolder = ls[index1]
 	ls[index1] = ls[index2]
 	ls[index2] = placeHolder
 
-def solve(ls):
+def solve(ls,solutions):
 	zeroIndex = None
 	heuristicValues = []
-	for i in range(len(ls)):
-		if ls[i] == 0:
-			zeroIndex = i
-	ls1 = ls.copy()
-	if zeroIndex - 1 >= 0:
-		swapValuesInList(ls1,zeroIndex,zeroIndex - 1)
-		ls1Heur = calculateHeuristic(ls1)
-		heuristicValues.append(ls1Heur)
+	validLists = []
+	heuristicValues = []
+	if calculateInversions(ls) != 0:
+		for i in range(len(ls)):
+			if ls[i] == 0:
+				zeroIndex = i
+		ls1 = ls.copy()
+		if zeroIndex - 1 >= 0:
+			swapValuesInList(ls1,zeroIndex,zeroIndex - 1)
+			ls1Heur = calculateHeuristic(ls1)
+			validLists.append(ls1)
+			heuristicValues.append(ls1Heur)
 
-	ls2 = ls.copy()
-	if zeroIndex + 1 < len(ls):
-		swapValuesInList(ls2,zeroIndex,zeroIndex + 1)
-		ls2Heur = calculateHeuristic(ls2)
-		heuristicValues.append(ls2Heur)
+		ls2 = ls.copy()
+		if zeroIndex + 1 < len(ls):
+			swapValuesInList(ls2,zeroIndex,zeroIndex + 1)
+			ls2Heur = calculateHeuristic(ls2)
+			validLists.append(ls2)
+			heuristicValues.append(ls2Heur)
 
-	ls3 = ls.copy()
-	if zeroIndex - 3 >= 0:
-		swapValuesInList(ls3,zeroIndex,zeroIndex - 3)
-		ls3Heur = calculateHeuristic(ls3)
-		heuristicValues.append(ls3Heur)
+		ls3 = ls.copy()
+		if zeroIndex - 3 >= 0:
+			swapValuesInList(ls3,zeroIndex,zeroIndex - 3)
+			ls3Heur = calculateHeuristic(ls3)
+			heuristicValues.append(ls3Heur)
+			validLists.append(ls3)
+			heuristicValues.append(ls3Heur)
 
-	ls4 = ls.copy()
-	if zeroIndex + 3 < len(ls):
-		swapValuesInList(ls4,zeroIndex,zeroIndex + 3)
-		ls4Heur = calculateHeuristic(ls4)
-		heuristicValues.append(ls4Heur)
+		ls4 = ls.copy()
+		if zeroIndex + 3 < len(ls):
+			swapValuesInList(ls4,zeroIndex,zeroIndex + 3)
+			ls4Heur = calculateHeuristic(ls4)
+			heuristicValues.append(ls4Heur)
+			validLists.append(ls4)
+			heuristicValues.append(ls4Heur)
 
-	minHeursitic = min(heuristicValues)
-	print(heuristicValues)
+		minIndex = heuristicValues.index(min(heuristicValues))
+		solve(validLists[minIndex],solutions)
 
 if __name__ == '__main__':
 	foo = createGame()
 	printBoard(foo)
-	print(calculateHeuristic(foo))
-	solve(foo)
+	boards = []
+	solve(foo,boards)
+	
