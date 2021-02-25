e35163a7c5608803d75526cce3426ad7ec587635
AbdelrahmanAbdelhalim <56049226+AbdelrahmanAbdelhalim@users.noreply.github.com>
2021-02-25 00:31:34 +0000

    initial writing for solve function

--- lab2.py
+++ lab2.py
@@ -21,7 +21,15 @@ def createGame():
 	else:
 		return createGame()
 
-
+def calculateHeuristic(ls):
+	heuristic = 0
+	for i in range(len(ls)):
+		if ls[i] == 0:
+			continue
+		else:
+			if i + 1 != ls[i]:
+				heuristic += 1
+	return heuristic
 def calculateInversions(ls):
 	inversions = 0
 	for foo in range(len(ls)):
@@ -31,7 +39,6 @@ def calculateInversions(ls):
 			if ls[lee] < ls[foo]:
 				inversions += 1
 	return inversions
-def findHeuristicValeu(ls):
 	
 def solvableGame(numOfInversions):
 	if numOfInversions % 2 == 0:
@@ -44,8 +51,46 @@ def printBoard(ls):
 		if k % 3 == 0:
 			print()
 		print(ls[k],end = ' ')
+def swapValuesInList(ls,index1,index2):
+	placeHolder = ls[index1]
+	ls[index1] = ls[index2]
+	ls[index2] = placeHolder
+
+def solve(ls):
+	zeroIndex = None
+	heuristicValues = []
+	for i in range(len(ls)):
+		if ls[i] == 0:
+			zeroIndex = i
+	ls1 = ls.copy()
+	if zeroIndex - 1 >= 0:
+		swapValuesInList(ls1,zeroIndex,zeroIndex - 1)
+	ls1Heur = calculateHeuristic(ls1)
+	heuristicValues.append(ls1Heur)
+
+	ls2 = ls.copy()
+	if zeroIndex + 1 < len(ls):
+		swapValuesInList(ls2,zeroIndex,zeroIndex + 1)
+		ls2Heur = calculateHeuristic(ls2)
+		heuristicValues.append(ls2Heur)
+
+	ls3 = ls.copy()
+	if zeroIndex - 3 >= 0:
+		swapValuesInList(ls3,zeroIndex,zeroIndex - 3)
+		ls3Heur = calculateHeuristic(ls3)
+		heuristicValues.append(ls3Heur)
+
+	ls4 = ls.copy()
+	if zeroIndex + 3 < len(ls):
+		swapValuesInList(ls4,zeroIndex,zeroIndex + 3)
+		ls4Heur = calculateHeuristic(ls4)
+		heuristicValues.append(ls4Heur)
+
+	minHeursitic = min(heuristicValues)
+	print(heuristicValues)
 
 if __name__ == '__main__':
 	foo = createGame()
 	printBoard(foo)
-	# solvePuzzle(foo)
+	print(calculateHeuristic(foo))
+	solve(foo)
