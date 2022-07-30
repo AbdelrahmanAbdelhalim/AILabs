accca5f4d43d8fc34852018e5ec5efae46bcd41b
Abdelrahman Abdelhalim <56049226+AbdelrahmanAbdelhalim@users.noreply.github.com>
2022-07-30 11:41:38 +0100

    Convert tabs to spaces

--- lab1.py
+++ lab1.py
@@ -4,61 +4,61 @@ import time
 
 """
 genRandomNumbers(minn, maxx,range):
-	Generate <<rangee>> Random numbers between minn and maxx 
+    Generate <<rangee>> Random numbers between minn and maxx 
 solve(ls,operation,target):
-	Finds the operations needed to reach the target sum using the numbers in ls
-	uses DFS to find the answer.
-	Works by brute forcing through every pair of numbers and performing the 4 arithmetic calculations
-	on them and then appending the result to the end of the array, then recursively calling the function
-	on the newly generated array.
+    Finds the operations needed to reach the target sum using the numbers in ls
+    uses DFS to find the answer.
+    Works by brute forcing through every pair of numbers and performing the 4 arithmetic calculations
+    on them and then appending the result to the end of the array, then recursively calling the function
+    on the newly generated array.
 
 """
 def genRandomNumbers(minn = 1,maxx = 13,rangee = 4):
-	ls = [rand.randint(minn,maxx) for i in range(rangee)]
-	return ls
-def solve(ls,operation,target):	
-	if len(ls) > 1:
-		for l in range(len(ls)):
-			for r in range(len(ls)):
-				if l == r:
-					continue
-				newLs = []
-				for j in range(len(ls)):
-					if j != l and j!= r:
-						newLs.append(ls[j])
+    ls = [rand.randint(minn,maxx) for i in range(rangee)]
+    return ls
+def solve(ls,operation,target):
+    if len(ls) > 1:
+        for l in range(len(ls)):
+            for r in range(len(ls)):
+                if l == r:
+                    continue
+                newLs = []
+                for j in range(len(ls)):
+                    if j != l and j!= r:
+                        newLs.append(ls[j])
 
-				newLs.append(ls[l] + ls[r])
-				operation.append(str(ls[l]) + " plus " + str(ls[r]))
-				solve(newLs,operation,target)
-				newLs = newLs[:-1]
-				operation.pop()
+                newLs.append(ls[l] + ls[r])
+                operation.append(str(ls[l]) + " plus " + str(ls[r]))
+                solve(newLs,operation,target)
+                newLs = newLs[:-1]
+                operation.pop()
 
-				newLs.append(ls[l] - ls[r])
-				operation.append(str(ls[l]) + " minus " + str(ls[r]))
-				solve(newLs,operation,target)
-				newLs = newLs[:-1]
-				operation.pop()
+                newLs.append(ls[l] - ls[r])
+                operation.append(str(ls[l]) + " minus " + str(ls[r]))
+                solve(newLs,operation,target)
+                newLs = newLs[:-1]
+                operation.pop()
 
-				newLs.append(ls[l] * ls[r])
-				operation.append(str(ls[l]) + " mul " + str(ls[r]))
-				solve(newLs,operation,target)
-				newLs = newLs[:-1]
-				operation.pop()
+                newLs.append(ls[l] * ls[r])
+                operation.append(str(ls[l]) + " mul " + str(ls[r]))
+                solve(newLs,operation,target)
+                newLs = newLs[:-1]
+                operation.pop()
 
-				if ls[r] != 0:
-					newLs.append(ls[l] / ls[r])
-					operation.append(str(ls[l]) + " divide " + str(ls[r]))
-					solve(newLs,operation,target)
-					newLs = newLs[:-1]
-					operation.pop()
-	if len(ls) == 1:
-		if abs(ls[0] - target) < 0.0000001:
-			print(operation)
+                if ls[r] != 0:
+                    newLs.append(ls[l] / ls[r])
+                    operation.append(str(ls[l]) + " divide " + str(ls[r]))
+                    solve(newLs,operation,target)
+                    newLs = newLs[:-1]
+                    operation.pop()
+    if len(ls) == 1:
+        if abs(ls[0] - target) < 0.0000001:
+            print(operation)
 
 if __name__ == "__main__":
-	start = time.time()
-	foo = genRandomNumbers(1,13,4)
-	print(foo)
-	solve(foo,[],24)
-	end = time.time()
-	print(f"Running time of Algorithm: {end - start}")
+    start = time.time()
+    foo = genRandomNumbers(1,13,4)
+    print(foo)
+    solve(foo,[],24)
+    end = time.time()
+    print(f"Running time of Algorithm: {end - start}")
--- lab2.py
+++ lab2.py
@@ -1,118 +1,118 @@
 import random as rand
 
 """
-	createGame():
-		creates a solvable 8 game puzzle by random
-		Not very efficiend, maybe needs a fix
+    createGame():
+        creates a solvable 8 game puzzle by random
+        Not very efficiend, maybe needs a fix
 """
-def createGame():	
-	#Odd inversions means unsolvable
-	#Create Puzzle with even inversions
-	ls = []
-	available = [i for i in range(1,9)]
-	for i in range(8):
-		nww = rand.randint(0,len(available) - 1	)
-		ls.append(available[nww])
-		available.pop(nww)
-	ls.append(0)
-	inversions = calculateInversions(ls)
-	if solvableGame(inversions):
-		return ls
-	else:
-		return createGame()
+def createGame():    
+    #Odd inversions means unsolvable
+    #Create Puzzle with even inversions
+    ls = []
+    available = [i for i in range(1,9)]
+    for i in range(8):
+        nww = rand.randint(0,len(available) - 1)
+        ls.append(available[nww])
+        available.pop(nww)
+    ls.append(0)
+    inversions = calculateInversions(ls)
+    if solvableGame(inversions):
+        return ls
+    else:
+        return createGame()
 
 def calculateHeuristic(ls,depth):
-	heuristic = 0
-	manDistance = 0
-	for i in range(len(ls)):
-		if ls[i] == 0:
-			continue
-		if i + 1 != ls[i]:
-			heuristic += 1
+    heuristic = 0
+    manDistance = 0
+    for i in range(len(ls)):
+        if ls[i] == 0:
+            continue
+        if i + 1 != ls[i]:
+            heuristic += 1
 
-	for i in range(len(ls)):
-		if ls[i] == 0 :
-			continue
-		goalSquare = ls[i] - 1
-		diffY = abs((int(i/3) - int(goalSquare/3)))
-		diffX = abs((i%3 - goalSquare%3))
-		manDistance += (diffX + diffY)
-	heuristic += manDistance
-	heuristic += depth
-	return heuristic
+    for i in range(len(ls)):
+        if ls[i] == 0 :
+            continue
+        goalSquare = ls[i] - 1
+        diffY = abs((int(i/3) - int(goalSquare/3)))
+        diffX = abs((i%3 - goalSquare%3))
+        manDistance += (diffX + diffY)
+    heuristic += manDistance
+    heuristic += depth
+    return heuristic
 
 def calculateInversions(ls):
-	inversions = 0
-	for l in range(len(ls)):
-		for r in range(l,len(ls)):
-			if ls[l] != 0 and ls[r] != 0:
-				if ls[l] > ls[r]:
-					inversions += 1
-	return inversions
-	
+    inversions = 0
+    for l in range(len(ls)):
+        for r in range(l,len(ls)):
+            if ls[l] != 0 and ls[r] != 0:
+                if ls[l] > ls[r]:
+                    inversions += 1
+    return inversions
+    
 def solvableGame(numOfInversions):
-	return numOfInversions % 2 == 0
+    return numOfInversions % 2 == 0
 
 def printBoard(ls):
-	for k in range(len(ls)):
-		if k % 3 == 0:
-			print()
-		print(ls[k],end = ' ')
-	print()
+    for k in range(len(ls)):
+        if k % 3 == 0:
+            print()
+        print(ls[k],end = ' ')
+    print()
 def swapValuesInList(ls,index1,index2):
-	placeHolder = ls[index1]
-	ls[index1] = ls[index2]
-	ls[index2] = placeHolder
+    placeHolder = ls[index1]
+    ls[index1] = ls[index2]
+    ls[index2] = placeHolder
 
 def solve(ls,depth,closedNodes,goal = [1,2,3,4,5,6,7,8,0]):
-	zeroIndex = None
-	closedNodes.append(ls)
-	validLists = []
-	printBoard(ls)
-	if ls != goal and solvableGame(calculateInversions(ls)):
-		for i in range(len(ls)):
-			if ls[i] == 0:
-				zeroIndex = i
-		ls1 = ls.copy()
+    zeroIndex = None
+    closedNodes.append(ls)
+    validLists = []
+    printBoard(ls)
+    if ls != goal and solvableGame(calculateInversions(ls)):
+        for i in range(len(ls)):
+            if ls[i] == 0:
+                zeroIndex = i
+        ls1 = ls.copy()
 
-		# Move Left
-		if (zeroIndex + 1)  % 3 != 1 :
-			swapValuesInList(ls1,zeroIndex,zeroIndex - 1)
-			if ls1 not in closedNodes:
-				validLists.append(ls1)
-		# Move Right
-		ls2 = ls.copy()
-		if (zeroIndex + 1) % 3 != 0:
-			swapValuesInList(ls2,zeroIndex,zeroIndex + 1)
-			if ls2 not in closedNodes:
-				validLists.append(ls2)
-		# Move Up
-		ls3 = ls.copy()
-		if zeroIndex - 3 >= 0:
-			swapValuesInList(ls3,zeroIndex,zeroIndex - 3)
-			if ls3 not in closedNodes:
-				validLists.append(ls3)
+        # Move Left
+        if (zeroIndex + 1)  % 3 != 1 :
+            swapValuesInList(ls1,zeroIndex,zeroIndex - 1)
+            if ls1 not in closedNodes:
+                validLists.append(ls1)
+        # Move Right
+        ls2 = ls.copy()
+        if (zeroIndex + 1) % 3 != 0:
+            swapValuesInList(ls2,zeroIndex,zeroIndex + 1)
+            if ls2 not in closedNodes:
+                validLists.append(ls2)
+        # Move Up
+        ls3 = ls.copy()
+        if zeroIndex - 3 >= 0:
+            swapValuesInList(ls3,zeroIndex,zeroIndex - 3)
+            if ls3 not in closedNodes:
+                validLists.append(ls3)
 
-		# Move Down
-		ls4 = ls.copy()
-		if zeroIndex + 3 < len(ls):
-			swapValuesInList(ls4,zeroIndex,zeroIndex + 3)
-			if ls4 not in closedNodes:
-				validLists.append(ls4)
+        # Move Down
+        ls4 = ls.copy()
+        if zeroIndex + 3 < len(ls):
+            swapValuesInList(ls4,zeroIndex,zeroIndex + 3)
+            if ls4 not in closedNodes:
+                validLists.append(ls4)
 
-		validLists = sorted(validLists,key = lambda x: calculateHeuristic(x,depth))
-		print(len(closedNodes))
-		depth += 1
-		solve(validLists[0],depth,closedNodes)
+        validLists = sorted(validLists,key = lambda x: calculateHeuristic(x,depth))
+        print(len(closedNodes))
+        depth += 1
+        solve(validLists[0],depth,closedNodes)
 
 
 if __name__ == '__main__':
-	foo = createGame()
-	printBoard(foo)
-	solve(foo,0,[])
-	print()
-	print("initial State:")
-	printBoard(foo)
-	# print(calculateInversions([5,2,8,4,1,7,3,6]))
-	# print(solvableGame(calculateInversions([1,2,3,7,6,5,0,4,8])))
-	
+    foo = createGame()
+    printBoard(foo)
+    solve(foo,0,[])
+    print()
+    print("initial State:")
+    printBoard(foo)
+    # print(calculateInversions([5,2,8,4,1,7,3,6]))
+    # print(solvableGame(calculateInversions([1,2,3,7,6,5,0,4,8])))
+    
